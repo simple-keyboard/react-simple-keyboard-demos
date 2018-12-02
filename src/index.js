b099c6d5ff9a48d1eb5a7ac64370101b0d2e9e02
Francisco Hodge <hello@franciscohodge.com>
2018-12-02 16:54:45 -0500

    full-keyboard

--- src/index.js
+++ src/index.js
@@ -10,6 +10,95 @@ class App extends Component {
     input: ""
   };
 
+  commonKeyboardOptions = {
+    onChange: input => this.onChange(input),
+    onKeyPress: button => this.onKeyPress(button),
+    theme: "simple-keyboard hg-theme-default hg-layout-default",
+    physicalKeyboardHighlight: true,
+    syncInstanceInputs: true,
+    mergeDisplay: true,
+    debug: true
+  };
+
+  keyboardOptions = {
+    ...this.commonKeyboardOptions,
+    /**
+     * Layout by:
+     * Sterling Butters (https://github.com/SterlingButters)
+     */
+    layout: {
+      default: [
+        "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+        "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+        "{tab} q w e r t y u i o p [ ] \\",
+        "{capslock} a s d f g h j k l ; ' {enter}",
+        "{shiftleft} z x c v b n m , . / {shiftright}",
+        "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+      ],
+      shift: [
+        "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+        "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
+        "{tab} Q W E R T Y U I O P { } |",
+        '{capslock} A S D F G H J K L : " {enter}',
+        "{shiftleft} Z X C V B N M < > ? {shiftright}",
+        "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+      ]
+    },
+    display: {
+      "{escape}": "esc ⎋",
+      "{tab}": "tab ⇥",
+      "{backspace}": "backspace ⌫",
+      "{enter}": "enter ↵",
+      "{capslock}": "caps lock ⇪",
+      "{shiftleft}": "shift ⇧",
+      "{shiftright}": "shift ⇧",
+      "{controlleft}": "ctrl ⌃",
+      "{controlright}": "ctrl ⌃",
+      "{altleft}": "alt ⌥",
+      "{altright}": "alt ⌥",
+      "{metaleft}": "cmd ⌘",
+      "{metaright}": "cmd ⌘"
+    }
+  };
+
+  keyboardControlPadOptions = {
+    ...this.commonKeyboardOptions,
+    layout: {
+      default: [
+        "{prtscr} {scrolllock} {pause}",
+        "{insert} {home} {pageup}",
+        "{delete} {end} {pagedown}"
+      ]
+    }
+  };
+
+  keyboardArrowsOptions = {
+    ...this.commonKeyboardOptions,
+    layout: {
+      default: ["{arrowup}", "{arrowleft} {arrowdown} {arrowright}"]
+    }
+  };
+
+  keyboardNumPadOptions = {
+    ...this.commonKeyboardOptions,
+    layout: {
+      default: [
+        "{numlock} {numpaddivide} {numpadmultiply}",
+        "{numpad7} {numpad8} {numpad9}",
+        "{numpad4} {numpad5} {numpad6}",
+        "{numpad1} {numpad2} {numpad3}",
+        "{numpad0} {numpaddecimal}"
+      ]
+    }
+  };
+
+  keyboardNumPadEndOptions = {
+    ...this.commonKeyboardOptions,
+    layout: {
+      default: ["{numpadsubtract}", "{numpadadd}", "{numpadenter}"]
+    }
+  };
+
   onChange = input => {
     this.setState({
       input: input
@@ -23,7 +112,14 @@ class App extends Component {
     /**
      * If you want to handle the shift and caps lock buttons
      */
-    if (button === "{shift}" || button === "{lock}") this.handleShift();
+    if (
+      button === "{shift}" ||
+      button === "{shiftleft}" ||
+      button === "{shiftright}" ||
+      button === "{capslock}"
+    ) {
+      this.handleShift();
+    }
   };
 
   handleShift = () => {
@@ -54,12 +150,40 @@ class App extends Component {
           placeholder={"Tap on the virtual keyboard to start"}
           onChange={e => this.onChangeInput(e)}
         />
-        <Keyboard
-          ref={r => (this.keyboard = r)}
-          layoutName={this.state.layoutName}
-          onChange={input => this.onChange(input)}
-          onKeyPress={button => this.onKeyPress(button)}
-        />
+        <div className={"keyboardContainer"}>
+          <Keyboard
+            baseClass={"simple-keyboard-main"}
+            ref={r => (this.keyboard = r)}
+            layoutName={this.state.layoutName}
+            {...this.keyboardOptions}
+          />
+
+          <div className="controlArrows">
+            <Keyboard
+              baseClass={"simple-keyboard-control"}
+              ref={r => (this.keyboard = r)}
+              {...this.keyboardControlPadOptions}
+            />
+            <Keyboard
+              baseClass={"simple-keyboard-arrows"}
+              ref={r => (this.keyboard = r)}
+              {...this.keyboardArrowsOptions}
+            />
+          </div>
+
+          <div className="numPad">
+            <Keyboard
+              baseClass={"simple-keyboard-numpad"}
+              ref={r => (this.keyboard = r)}
+              {...this.keyboardNumPadOptions}
+            />
+            <Keyboard
+              baseClass={"simple-keyboard-numpadEnd"}
+              ref={r => (this.keyboard = r)}
+              {...this.keyboardNumPadEndOptions}
+            />
+          </div>
+        </div>
       </div>
     );
   }
