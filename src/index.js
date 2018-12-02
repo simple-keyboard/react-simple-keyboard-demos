86cd4bee18959292f2b67326ffb29a31c67d4fba
Francisco Hodge <hello@franciscohodge.com>
2018-12-02 16:48:31 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
+import "./index.css";
 
 class App extends Component {
   state = {
@@ -45,20 +46,11 @@ class App extends Component {
     );
   };
 
-  inputStyle = {
-    width: "100%",
-    height: "100px",
-    padding: "10px",
-    fontSize: 20,
-    border: 0
-  };
-
   render() {
     return (
       <div>
         <input
           value={this.state.input}
-          style={this.inputStyle}
           placeholder={"Tap on the virtual keyboard to start"}
           onChange={e => this.onChangeInput(e)}
         />
