d6baa34735b629cece3dee3dbf2f812f0745948f
Francisco Hodge <hello@franciscohodge.com>
2018-12-02 23:05:09 -0500

    init

--- src/index.js
+++ src/index.js
@@ -56,9 +56,36 @@ class App extends Component {
         />
         <Keyboard
           ref={r => (this.keyboard = r)}
-          layoutName={this.state.layoutName}
           onChange={input => this.onChange(input)}
           onKeyPress={button => this.onKeyPress(button)}
+          theme={"hg-theme-default hg-layout-default myTheme"}
+          layoutName={this.state.layoutName}
+          layout={{
+            default: [
+              "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+              "{tab} q w e r t y u i o p [ ] \\",
+              "{lock} a s d f g h j k l ; ' {enter}",
+              "{shift} z x c v b n m , . / {shift}",
+              ".com @ {space}"
+            ],
+            shift: [
+              "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+              "{tab} Q W E R T Y U I O P { } |",
+              '{lock} A S D F G H J K L : " {enter}',
+              "{shift} Z X C V B N M < > ? {shift}",
+              ".com @ {space}"
+            ]
+          }}
+          buttonTheme={[
+            {
+              class: "hg-red",
+              buttons: "Q W E R T Y q w e r t y"
+            },
+            {
+              class: "hg-highlight",
+              buttons: "Q q"
+            }
+          ]}
         />
       </div>
     );
