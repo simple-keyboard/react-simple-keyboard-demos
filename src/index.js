42691efa8cecf39110dffe137d2d7e501c996aa0
Francisco Hodge <hello@franciscohodge.com>
2019-03-07 22:52:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ class App extends Component {
         input: input
       },
       () => {
-        this.keyboard.setInput(input);
+        this.keyboardRef.keyboard.setInput(input);
       }
     );
   };
@@ -68,7 +68,7 @@ class App extends Component {
           onChange={e => this.onChangeInput(e)}
         />
         <Keyboard
-          ref={r => (this.keyboard = r)}
+          ref={r => (this.keyboardRef = r)}
           layoutName={this.state.layoutName}
           layout={{
             default: [
