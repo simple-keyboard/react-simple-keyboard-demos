fa2110c1077f9a9b63c5147350707758a61097df
Francisco Hodge <hello@franciscohodge.com>
2019-03-10 13:27:10 -0400

    uc-button-container

--- src/index.js
+++ src/index.js
@@ -59,6 +59,28 @@ class App extends Component {
           layoutName={this.state.layoutName}
           onChange={input => this.onChange(input)}
           onKeyPress={button => this.onKeyPress(button)}
+          layout={{
+            'default': [
+              '` [1 2 3 4 5 6 7 8 9] 0 - = {bksp}',
+              '{tab} q w e r t y u [i o p] [ ] \\',
+              '{lock} [a s d] f g h j k l ; \' {enter}',
+              '{shift} z x c v b n m , . / {shift}',
+              '[.com @] {space} {arrowleft} [{arrowup} {arrowdown}] {arrowright}'
+            ],
+            'shift': [
+              '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+              '{tab} Q W E R T Y U I O P { } |',
+              '{lock} A S D F G H J K L : " {enter}',
+              '{shift} Z X C V B N M < > ? {shift}',
+              '.com @ {space}'
+            ]
+          }}
+          buttonTheme={[
+            {
+              class: "myArrowBtns",
+              buttons: "{arrowleft} [{arrowup} {arrowdown}] {arrowright}"
+            }
+          ]}
         />
       </div>
     );
