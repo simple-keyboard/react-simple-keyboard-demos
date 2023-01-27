c1264a5b877261f22ab5cd29297ca9677bbf6aaf
Francisco Hodge <hello@franciscohodge.com>
2023-01-27 04:36:50 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import Keyboard from "react-simple-keyboard";
+
+// Instead of the default import, you can also use this:
+// import { KeyboardReact as Keyboard } from "react-simple-keyboard"
+
 import "react-simple-keyboard/build/css/index.css";
 import "./index.css";
 
